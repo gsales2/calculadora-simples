3aeff7a0129da076573e77418193bc5b4c7367ff
Gabriel Sales <gabrielsales2002@gmail.com>
2026-01-05 16:24:31 -0300

    faz logico dos botoes numerais, C, CE e inicio da logica dos operadores

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const botoes = document.querySelectorAll('.botao')
+const botoes = document.querySelectorAll('.botao');
+const display = document.querySelector('input')
+let valorAtual = "";
+let valorAntigo = ""
+
+botoes.forEach((botao) => {
+    botao.addEventListener('click', () => {
+        const tipo = botao.dataset.type;
+        const valor = botao.dataset.value;
+        if (tipo === 'number') {
+            let valorClicado = valor;
+            valorAtual += valorClicado;
+            display.value = valorAtual
+        }
+        if ( valor === 'clear') {
+            valorAtual = '';
+            display.value = valorAtual
+        }
+        if ( valor === 'clear-entry') {
+            valorAtual = valorAtual.slice(0, -1)
+            display.value = valorAtual
+        }
+        if (tipo === 'operator') {
+            valorAntigo = valorAtual
+            valorAtual = ""
+            display.value = valorAtual
+        }
+    });
+})
