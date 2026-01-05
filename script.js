2d033154319000267a7e12ed489d9d7015607409
Gabriel Sales <gabrielsales2002@gmail.com>
2026-01-05 17:15:36 -0300

    Adiciona mensagem de erro para divisao por 0

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
-const botoes = document.querySelectorAll('.botao');
-const display = document.querySelector('input')
+const botoes = document.querySelectorAll(".botao");
+const display = document.querySelector("input");
 let valorAtual = "";
 let valorAntigo = "";
 let operadorAtual = "";
 let resultado = "";
 
 botoes.forEach((botao) => {
-    botao.addEventListener('click', () => {
-        const tipo = botao.dataset.type;
-        const valor = botao.dataset.value;
-        if (tipo === 'number') {
-            let valorClicado = valor;
-            valorAtual += valorClicado;
-            display.value = valorAtual
-        }
-        if ( valor === 'clear') {
-            valorAtual = '';
-            display.value = valorAtual
-        }
-        if ( valor === 'clear-entry') {
-            valorAtual = valorAtual.slice(0, -1)
-            display.value = valorAtual
-        }
-        if (tipo === 'operator') {
-            operadorAtual = valor
-            valorAntigo = valorAtual
-            valorAtual = ""
-            display.value = valorAtual
-        }
-        if (tipo === 'result') {
-            let valorAntigoFormatado = parseInt(valorAntigo)
-            let valorAtualFormatado = parseInt(valorAtual)
-            switch(operadorAtual) {
-                case '+':
-                    resultado = valorAntigoFormatado + valorAtualFormatado
-                    break;
-                case '-':
-                    resultado = valorAntigoFormatado - valorAtualFormatado
-                    break;
-                case '/':
-                    resultado = valorAntigoFormatado / valorAtualFormatado
-                    break;
-                case '*':
-                   resultado = valorAntigoFormatado * valorAtualFormatado
-                    break;
-            }
-            valorAtual = resultado
-            valorAntigo = ""
-            operadorAtual = ""
-            display.value = valorAtual
-        }
-    });
-})
+  botao.addEventListener("click", () => {
+    const tipo = botao.dataset.type;
+    const valor = botao.dataset.value;
+    if (tipo === "number") {
+      let valorClicado = valor;
+      valorAtual += valorClicado;
+      display.value = valorAtual;
+    } else if (tipo === "action" && valor === "clear") {
+      valorAtual = "";
+      display.value = valorAtual;
+    } else if (tipo === "action" && valor === "clear-entry") {
+      valorAtual = valorAtual.slice(0, -1);
+      display.value = valorAtual;
+    } else if (tipo === "operator") {
+      operadorAtual = valor;
+      valorAntigo = valorAtual;
+      valorAtual = "";
+      display.value = operadorAtual;
+    } else if (tipo === "result") {
+      let valorAntigoFormatado = Number(valorAntigo);
+      let valorAtualFormatado = Number(valorAtual);
+      if (operadorAtual === "/" && valorAtualFormatado === 0) {
+        display.value = "Erro: divisão por 0";
+        valorAtual = "";
+        valorAntigo = "";
+        operadorAtual = "";
+        return;
+      }
+      switch (operadorAtual) {
+        case "+":
+          resultado = valorAntigoFormatado + valorAtualFormatado;
+          break;
+        case "-":
+          resultado = valorAntigoFormatado - valorAtualFormatado;
+          break;
+        case "/":
+          resultado = valorAntigoFormatado / valorAtualFormatado;
+          break;
+        case "*":
+          resultado = valorAntigoFormatado * valorAtualFormatado;
+          break;
+      }
+      valorAtual = resultado;
+      valorAntigo = "";
+      operadorAtual = "";
+      display.value = valorAtual;
+    }
+  });
+});
